3974c8fd61159f70d04da369451d573ca8c5918d
Ozair Shafique <bc090402541@gmail.com>
2026-04-25 19:51:00 +0200

    Add POST request handler for authentication with password validation

---
+++ app/api/auth/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { password } = await request.json();
+    if (password === process.env.ADMIN_SCERET) {
+      return NextResponse.json({ result: "success" });
+    } else {
+      return NextResponse.json({ error: "Wrong password" }, { status: 401 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
