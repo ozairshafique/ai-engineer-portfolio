327a8fb106286d3223e5160689ac6b675825e6fc
Ozair Shafique <bc090402541@gmail.com>
2026-04-26 12:39:23 +0200

    add ContactPages component to render the Contact section

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from "@/components/Contacts";
+
+export default function ContactPages() {
+  return (
+    <div className="min-h-screen pt-14">
+      <Contact />
+    </div>
+  );
+}
