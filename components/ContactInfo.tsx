39e9f569cb12d3aa922105cc0285a47d74ed2036
Ozair Shafique <bc090402541@gmail.com>
2026-05-05 17:06:48 +0200

    add phone component and update contact links

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -33,6 +33,24 @@ function GitHubIcon() {
     </svg>
   );
 }
+
+function PhoneIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.82a16 16 0 0 0 6.29 6.29l1.12-.93a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7a2 2 0 0 1 1.72 2.03z" />
+    </svg>
+  );
+}
+
 const contactLinks = [
   {
     label: "uzairi_11@hotmail.com",
@@ -48,6 +66,12 @@ const contactLinks = [
     label: "github.com/ozairshafique",
     href: "https://github.com/ozairshafique",
     icon: <GitHubIcon />,
+  },
+
+  {
+    label: "+39 348 048 3117",
+    href: "tel:+393480483117",
+    icon: <PhoneIcon />,
   },
 ];
 const availability = [
